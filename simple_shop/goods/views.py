7aa14c0ec093440bc31e14552032016222c5441f
Alexander Vinogradov <dra-katt@yandex.ru>
2020-02-02 21:33:16 +0300

    goods/views.py cleanup

--- simple_shop/goods/views.py
+++ simple_shop/goods/views.py
@@ -5,16 +5,6 @@ from django.http import HttpResponse
 from .models import Item, ItemType
 
 
-def index(request):
-    template = 'goods/index.html'
-    items = Item.objects.all().prefetch_related()
-    phones = items.filter(type=1).order_by('add_time')[:3]
-    # print('phones: ', phones)
-    accessoires = items.filter(type=2)[:3]
-    # print('accs', accessoires)
-    context = {'phones': phones,
-               'accessoires': accessoires}
-    return render(request, template, context)
 
 def login(request):
     template = 'goods/login.html'
@@ -26,20 +16,6 @@ def cart(request):
     context = {}
     return render(request, template, context)
 
-# def phones(request):
-#     template = 'goods/smartphones.html'
-#     phones = Item.objects.all().prefetch_related()\
-#         .filter(type=1).order_by('-add_time')
-#     context = {'phones': phones}
-#     return render(request, template, context)
-#
-# def accessoires(request):
-#     template = 'goods/accessoires.html'
-#     accessoires = Item.objects.all().prefetch_related()\
-#         .filter(type=2).order_by('-add_time')
-#     context = {'accessoires': accessoires}
-#     return render(request, template, context)
-    # return HttpResponse('accessoires collection')
 
 def items_by_category(request, item_type):
     page_num = int(request.GET.get('page', 1))
